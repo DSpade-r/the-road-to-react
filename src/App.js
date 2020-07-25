c59468fb0891ef7e33d342fcba0f36230883dc83
Dennis Spade <stalk.sdv@gmail.com>
2020-07-25 10:15:20 +0300

    Imperative React

--- src/App.js
+++ src/App.js
@@ -46,6 +46,7 @@ const App = () => {
       <InputWithLabel
         id='search'
         value={searchTerm}
+        isFocused
         onInputChange={handleSearch}
       >
         <strong>Search</strong>
@@ -58,17 +59,32 @@ const App = () => {
 
 const InputWithLabel = ({
   id,
-  label,
   value,
   type = 'text',
   onInputChange,
+  isFocused,
   children
-}) => (
-  <>
-    <label htmlFor={id}>{children}</label>&nbsp;
-    <input id={id} type={type} value={value} onChange={onInputChange} />
-  </>
-)
+}) => {
+  const inputRef = React.useRef()
+  React.useEffect(() => {
+    if (isFocused && inputRef.current) {
+      inputRef.current.focus()
+    }
+  }, [isFocused])
+  return (
+    <>
+      <label htmlFor={id}>{children}</label>&nbsp;
+      <input
+        ref={inputRef}
+        id={id}
+        type={type}
+        value={value}
+        autoFocus={isFocused}
+        onChange={onInputChange}
+      />
+    </>
+  )
+}
 
 const List = ({ list }) =>
   list.map(({ objectID, ...item }) => <Item key={objectID} {...item} />)
