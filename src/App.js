6d49dd1ab6b75e61b67e274fb27e05d17b7e991c
Dennis Spade <stalk.sdv@gmail.com>
2020-07-26 22:48:26 +0300

    Memoized Handler in React (Advanced)

--- src/App.js
+++ src/App.js
@@ -52,7 +52,8 @@ const App = () => {
     isLoading: false,
     isError: false
   })
-  React.useEffect(() => {
+
+  const handleFetchStories = React.useCallback(() => {
     if (searchTerm === '') return
     dispatchStories({ type: 'STORIES_FETCH_INIT' })
     fetch(`${API_ENDPOINT}${searchTerm}`)
@@ -65,6 +66,10 @@ const App = () => {
       })
       .catch(() => dispatchStories({ type: 'STORIES_FETCH_FAILURE' }))
   }, [searchTerm])
+
+  React.useEffect(() => {
+    handleFetchStories()
+  }, [handleFetchStories])
 
   const handleRemoveStory = item => {
     dispatchStories({ type: 'REMOVE_STORY', payload: item })
