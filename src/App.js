c400671a5a43a6ffe2919845b34a8f5639fc0ee5
Dennis Spade <stalk.sdv@gmail.com>
2020-07-26 23:18:08 +0300

    Third-Party Libraries in React

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import axios from 'axios'
 
 const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?query='
 
@@ -57,12 +58,12 @@ const App = () => {
   const handleFetchStories = React.useCallback(() => {
     if (url === '') return
     dispatchStories({ type: 'STORIES_FETCH_INIT' })
-    fetch(url)
-      .then(response => response.json())
+    axios
+      .get(url)
       .then(result => {
         dispatchStories({
           type: 'STORIES_FETCH_SUCCESS',
-          payload: result.hits
+          payload: result.data.hits
         })
       })
       .catch(() => dispatchStories({ type: 'STORIES_FETCH_FAILURE' }))
