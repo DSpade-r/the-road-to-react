762c6504d51b61879d54a0e931c26ead700fedf5
Dennis Spade <stalk.sdv@gmail.com>
2020-07-26 23:10:09 +0300

    Explicit Data Fetching with React

--- src/App.js
+++ src/App.js
@@ -47,6 +47,7 @@ const storiesReducer = (state, action) => {
 
 const App = () => {
   const [searchTerm, setSearchTerm] = useSemiPersistentState('search', 'React')
+  const [url, setUrl] = React.useState(`${API_ENDPOINT}${searchTerm}`)
   const [stories, dispatchStories] = React.useReducer(storiesReducer, {
     data: [],
     isLoading: false,
@@ -54,9 +55,9 @@ const App = () => {
   })
 
   const handleFetchStories = React.useCallback(() => {
-    if (searchTerm === '') return
+    if (url === '') return
     dispatchStories({ type: 'STORIES_FETCH_INIT' })
-    fetch(`${API_ENDPOINT}${searchTerm}`)
+    fetch(url)
       .then(response => response.json())
       .then(result => {
         dispatchStories({
@@ -65,7 +66,7 @@ const App = () => {
         })
       })
       .catch(() => dispatchStories({ type: 'STORIES_FETCH_FAILURE' }))
-  }, [searchTerm])
+  }, [url])
 
   React.useEffect(() => {
     handleFetchStories()
@@ -75,8 +76,12 @@ const App = () => {
     dispatchStories({ type: 'REMOVE_STORY', payload: item })
   }
 
-  const handleSearch = event => {
+  const handleSearchInput = event => {
     setSearchTerm(event.target.value)
+  }
+
+  const handleSearchSubmit = () => {
+    setUrl(`${API_ENDPOINT}${searchTerm}`)
   }
 
   return (
@@ -86,10 +91,13 @@ const App = () => {
         id='search'
         value={searchTerm}
         isFocused
-        onInputChange={handleSearch}
+        onInputChange={handleSearchInput}
       >
         <strong>Search</strong>
       </InputWithLabel>
+      <button type='button' disabled={!searchTerm} onClick={handleSearchSubmit}>
+        Submit
+      </button>
       <hr />
       {stories.isError && <p>Something went wrong...</p>}
       {stories.isLoading ? (
