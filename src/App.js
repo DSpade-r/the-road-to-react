4f723838d5f5a39d668f4586b862ad9dd48c1aa9
Dennis Spade <stalk.sdv@gmail.com>
2020-07-27 22:29:02 +0300

    Forms in React

--- src/App.js
+++ src/App.js
@@ -80,24 +80,19 @@ const App = () => {
     setSearchTerm(event.target.value)
   }
 
-  const handleSearchSubmit = () => {
+  const handleSearchSubmit = event => {
     setUrl(`${API_ENDPOINT}${searchTerm}`)
+    event.preventDefault()
   }
 
   return (
     <div>
       <h1>My Hacker Stories</h1>
-      <InputWithLabel
-        id='search'
-        value={searchTerm}
-        isFocused
-        onInputChange={handleSearchInput}
-      >
-        <strong>Search</strong>
-      </InputWithLabel>
-      <button type='button' disabled={!searchTerm} onClick={handleSearchSubmit}>
-        Submit
-      </button>
+      <SearchForm
+        searchTerm={searchTerm}
+        onSearchInput={handleSearchInput}
+        onSearchSubmit={handleSearchSubmit}
+      />
       <hr />
       {stories.isError && <p>Something went wrong...</p>}
       {stories.isLoading ? (
@@ -108,6 +103,22 @@ const App = () => {
     </div>
   )
 }
+
+const SearchForm = ({ searchTerm, onSearchInput, onSearchSubmit }) => (
+  <form onSubmit={onSearchSubmit}>
+    <InputWithLabel
+      id='search'
+      value={searchTerm}
+      isFocused
+      onInputChange={onSearchInput}
+    >
+      <strong>Search:</strong>
+    </InputWithLabel>
+    <button type='submit' disabled={!searchTerm}>
+      Submit
+    </button>
+  </form>
+)
 
 const InputWithLabel = ({
   id,
