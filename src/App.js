b0f3fb4adf19979a2d231645b957cd117dc8f071
Dennis Spade <stalk.sdv@gmail.com>
2020-07-28 09:13:59 +0300

    SVGs in React

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import axios from 'axios'
-import styled from 'styled-components'
 
 import './App.css'
+import { ReactComponent as Check } from './check.svg'
 
 const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?query='
 
@@ -10,9 +10,11 @@ const useSemiPersistentState = (key, initialState) => {
   const [value, setValue] = React.useState(
     localStorage.getItem(key) || initialState
   )
+
   React.useEffect(() => {
     localStorage.setItem(key, value)
   }, [value, key])
+
   return [value, setValue]
 }
 
@@ -51,7 +53,9 @@ const storiesReducer = (state, action) => {
 
 const App = () => {
   const [searchTerm, setSearchTerm] = useSemiPersistentState('search', 'React')
+
   const [url, setUrl] = React.useState(`${API_ENDPOINT}${searchTerm}`)
+
   const [stories, dispatchStories] = React.useReducer(storiesReducer, {
     data: [],
     isLoading: false,
@@ -60,8 +64,10 @@ const App = () => {
 
   const handleFetchStories = React.useCallback(async () => {
     dispatchStories({ type: 'STORIES_FETCH_INIT' })
+
     try {
       const result = await axios.get(url)
+
       dispatchStories({
         type: 'STORIES_FETCH_SUCCESS',
         payload: result.data.hits
@@ -76,7 +82,10 @@ const App = () => {
   }, [handleFetchStories])
 
   const handleRemoveStory = item => {
-    dispatchStories({ type: 'REMOVE_STORY', payload: item })
+    dispatchStories({
+      type: 'REMOVE_STORY',
+      payload: item
+    })
   }
 
   const handleSearchInput = event => {
@@ -85,29 +94,33 @@ const App = () => {
 
   const handleSearchSubmit = event => {
     setUrl(`${API_ENDPOINT}${searchTerm}`)
+
     event.preventDefault()
   }
 
   return (
-    <StyledContainer>
-      <StyledHeadlinePrimary>My Hacker Stories</StyledHeadlinePrimary>
+    <div className='container'>
+      <h1 className='headline-primary'>My Hacker Stories</h1>
+
       <SearchForm
         searchTerm={searchTerm}
         onSearchInput={handleSearchInput}
         onSearchSubmit={handleSearchSubmit}
       />
-      {stories.isError && <p>Something went wrong...</p>}
+
+      {stories.isError && <p>Something went wrong ...</p>}
+
       {stories.isLoading ? (
-        <p>Loading...</p>
+        <p>Loading ...</p>
       ) : (
         <List list={stories.data} onRemoveItem={handleRemoveStory} />
       )}
-    </StyledContainer>
+    </div>
   )
 }
 
 const SearchForm = ({ searchTerm, onSearchInput, onSearchSubmit }) => (
-  <StyledSearchForm onSubmit={onSearchSubmit}>
+  <form onSubmit={onSearchSubmit} className='search-form'>
     <InputWithLabel
       id='search'
       value={searchTerm}
@@ -116,10 +129,15 @@ const SearchForm = ({ searchTerm, onSearchInput, onSearchSubmit }) => (
     >
       <strong>Search:</strong>
     </InputWithLabel>
-    <StyledButtonLarge type='submit' disabled={!searchTerm}>
+
+    <button
+      type='submit'
+      disabled={!searchTerm}
+      className='button button_large'
+    >
       Submit
-    </StyledButtonLarge>
-  </StyledSearchForm>
+    </button>
+  </form>
 )
 
 const InputWithLabel = ({
@@ -131,21 +149,26 @@ const InputWithLabel = ({
   children
 }) => {
   const inputRef = React.useRef()
+
   React.useEffect(() => {
-    if (isFocused && inputRef.current) {
+    if (isFocused) {
       inputRef.current.focus()
     }
   }, [isFocused])
+
   return (
     <>
-      <StyledLabel htmlFor={id}>{children}</StyledLabel>
+      <label htmlFor={id} className='label'>
+        {children}
+      </label>
       &nbsp;
-      <StyledInput
+      <input
         ref={inputRef}
         id={id}
         type={type}
         value={value}
         onChange={onInputChange}
+        className='input'
       />
     </>
   )
@@ -157,82 +180,23 @@ const List = ({ list, onRemoveItem }) =>
   ))
 
 const Item = ({ item, onRemoveItem }) => (
-  <StyledItem>
-    <StyledColumn width='40%'>
+  <div className='item'>
+    <span style={{ width: '40%' }}>
       <a href={item.url}>{item.title}</a>
-    </StyledColumn>
-    <StyledColumn width='30%'>{item.author}</StyledColumn>
-    <StyledColumn width='10%'>{item.num_comments}</StyledColumn>
-    <StyledColumn width='10%'>{item.points}</StyledColumn>
-    <StyledColumn width='10%'>
-      <StyledButtonSmall type='button' onClick={() => onRemoveItem(item)}>
-        Dismiss
-      </StyledButtonSmall>
-    </StyledColumn>
-  </StyledItem>
+    </span>
+    <span style={{ width: '30%' }}>{item.author}</span>
+    <span style={{ width: '10%' }}>{item.num_comments}</span>
+    <span style={{ width: '10%' }}>{item.points}</span>
+    <span style={{ width: '10%' }}>
+      <button
+        type='button'
+        onClick={() => onRemoveItem(item)}
+        className='button button_small'
+      >
+        <Check height='18px' width='18px' />
+      </button>
+    </span>
+  </div>
 )
-
-const StyledContainer = styled.div`
-  height: 100vw;
-  padding: 20px;
-  background: #83a4d4;
-  background: linear-gradient(to left, #b6fbff, #83a4d4);
-  color: #171212;
-`
-const StyledHeadlinePrimary = styled.h1`
-  font-size: 48px;
-  font-weight: 300;
-  letter-spacing: 2px;
-`
-const StyledItem = styled.div`
-  display: flex;
-  align-items: center;
-  padding-bottom: 5px;
-`
-const StyledColumn = styled.span`
-  padding: 0 5px;
-  white-space: nowrap;
-  overflow: hidden;
-  white-space: nowrap;
-  text-overflow: ellipsis;
-  a {
-    color: inherit;
-  }
-  width: ${props => props.width};
-`
-const StyledButton = styled.button`
-  background: transparent;
-  border: 1px solid #171212;
-  padding: 5px;
-  cursor: pointer;
-  transition: all 0.1s ease-in;
-  &:hover {
-    background: #171212;
-    color: #ffffff;
-  }
-`
-const StyledButtonSmall = styled(StyledButton)`
-  padding: 5px;
-`
-const StyledButtonLarge = styled(StyledButton)`
-  padding: 10px;
-`
-const StyledSearchForm = styled.form`
-  padding: 10px 0 20px 0;
-  display: flex;
-  align-items: baseline;
-`
-const StyledLabel = styled.label`
-  border-top: 1px solid #171212;
-  border-left: 1px solid #171212;
-  padding-left: 5px;
-  font-size: 24px;
-`
-const StyledInput = styled.input`
-  border: none;
-  border-bottom: 1px solid #171212;
-  background-color: transparent;
-  font-size: 24px;
-`
 
 export default App
